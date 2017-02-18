557ee681e391869610415c3c28780147823376ad
Pauli Jokela <pauli.jokela@didstopia.com>
2017-02-18 10:27:28 +0200

    Added a way to override webrcon hostname for the website

--- fix_conn.sh
+++ fix_conn.sh
@@ -5,6 +5,9 @@
 # Replace the trimmed address field with the current domain/hostname and RCON port
 OLD_TRIM_CODE='scope.Address = $scope.Address.trim();'
 NEW_ADDRESS_CODE='scope.Address = $location.host() + '"\":$RUST_RCON_PORT\";"
+if [ ! -z "$RUST_RCON_HOSTNAME" ]; then
+	NEW_ADDRESS_CODE='scope.Address = '"\"$RUST_RCON_HOSTNAME:$RUST_RCON_PORT\";"
+fi
 sed -i -e 's/'"$OLD_TRIM_CODE"'/'"$NEW_ADDRESS_CODE"'/g' /usr/share/nginx/html/js/connection.js
 
 # Replace the help text with our own
