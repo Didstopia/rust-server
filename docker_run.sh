9d5c14c9ab1772fc942d40039d27d172ebaad33a
Pauli Jokela <pauli.jokela@didstopia.com>
2020-06-16 11:33:36 +0300

    Fixed typo in start_rust.sh

--- docker_run.sh
+++ docker_run.sh
@@ -6,7 +6,7 @@
 # docker run -p 0.0.0.0:28015:28015 -p 0.0.0.0:28015:28015/udp -p 0.0.0.0:28016:28016 -p 0.0.0.0:8080:8080 --name rust-server -it didstopia/rust-server:latest
 
 # Run a vanilla server
-docker run -p 0.0.0.0:28015:28015 -p 0.0.0.0:28015:28015/udp -p 0.0.0.0:28016:28016 -p 0.0.0.0:8080:8080 -v $(pwd)/rust_data:/steamcmd/rust --name rust-server -it --rm didstopia/rust-server:latest
+docker run -p 0.0.0.0:28015:28015 -p 0.0.0.0:28015:28015/udp -p 0.0.0.0:28016:28016 -p 0.0.0.0:8080:8080 -v $(pwd)/rust_data:/steamcmd/rust $@ --name rust-server -it --rm didstopia/rust-server:latest
 #docker run -p 0.0.0.0:28015:28015 -p 0.0.0.0:28015:28015/udp -p 0.0.0.0:28016:28016 -p 0.0.0.0:8080:8080 -e RUST_SERVER_STARTUP_ARGUMENTS="-batchmode -load -nographics +server.secure 1 -logfile /dev/null" -e RUST_START_MODE="2" -v $(pwd)/rust_data:/steamcmd/rust --name rust-server -it --rm didstopia/rust-server:latest
 
 # Run a modded server
